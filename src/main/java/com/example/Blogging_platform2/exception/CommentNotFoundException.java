63150ab9feab1df7bb0fb9331a52eda0114efa29
kakwezi peace <peacekakwezi222@gmail.com>
2026-01-30 10:18:32 +0200

    Exception CommentNotFound

--- src/main/java/com/example/Blogging_platform2/exception/CommentNotFoundException.java
+++ src/main/java/com/example/Blogging_platform2/exception/CommentNotFoundException.java
@@ -1,5 +1,6 @@
 package com.example.Blogging_platform2.exception;
 
+
 public class CommentNotFoundException extends RuntimeException {
     public CommentNotFoundException(int id) {
         super("Comment with ID " + id + " not found");
