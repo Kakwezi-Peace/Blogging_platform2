3994bbdce026ac4dab2149ea6d50a41d3c89eb1c
kakwezi peace <peacekakwezi222@gmail.com>
2026-01-30 10:24:55 +0200

    Update PostViewNotFoundException

--- src/main/java/com/example/Blogging_platform2/exception/PostViewNotFoundException.java
+++ src/main/java/com/example/Blogging_platform2/exception/PostViewNotFoundException.java
@@ -1,6 +1,8 @@
 package com.example.Blogging_platform2.exception;
 
 
+
+
 public class PostViewNotFoundException extends RuntimeException {
     public PostViewNotFoundException(int id) {
         super("PostView with ID " + id + " not found");
