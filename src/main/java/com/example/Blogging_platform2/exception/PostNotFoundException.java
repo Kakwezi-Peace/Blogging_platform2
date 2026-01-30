114ecdccaf6c820dac14b03bffa0003e398fd9e2
kakwezi peace <peacekakwezi222@gmail.com>
2026-01-30 10:20:49 +0200

    Creating PostNotFoundException

--- src/main/java/com/example/Blogging_platform2/exception/PostNotFoundException.java
+++ src/main/java/com/example/Blogging_platform2/exception/PostNotFoundException.java
@@ -1,5 +1,6 @@
 package com.example.Blogging_platform2.exception;
 
+
 public class PostNotFoundException extends RuntimeException {
     public PostNotFoundException(int id) {
         super("Post with ID " + id + " not found");
