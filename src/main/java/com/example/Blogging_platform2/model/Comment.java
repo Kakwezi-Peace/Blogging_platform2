ba403858623172e5d25cb00377b9c26fd364151c
kakwezi peace <peacekakwezi222@gmail.com>
2026-01-30 10:13:42 +0200

    Adding a comment class

--- src/main/java/com/example/Blogging_platform2/model/Comment.java
+++ src/main/java/com/example/Blogging_platform2/model/Comment.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Comment {
+    // fields
     private int commentId;
     private int postId;
     private int userId;
